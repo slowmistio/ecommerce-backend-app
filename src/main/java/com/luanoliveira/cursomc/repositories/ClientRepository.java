04f8a1fbcdba24d2a672f6bbe148309f7ac1775b
Luan Oliveira <luannn@gmail.com>
2018-04-01 12:33:06 -0300

    Adicionada validação do email para não adicionar valor repetido

--- src/main/java/com/luanoliveira/cursomc/repositories/ClientRepository.java
+++ src/main/java/com/luanoliveira/cursomc/repositories/ClientRepository.java
@@ -2,10 +2,14 @@ package com.luanoliveira.cursomc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.luanoliveira.cursomc.domain.Client;
 
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Integer>{
-
+	
+	@Transactional(readOnly = true)
+	Client findByEmail(String email);
+	
 }
--- src/main/java/com/luanoliveira/cursomc/services/validation/ClientInsertValidator.java
+++ src/main/java/com/luanoliveira/cursomc/services/validation/ClientInsertValidator.java
@@ -6,12 +6,19 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.luanoliveira.cursomc.domain.Client;
 import com.luanoliveira.cursomc.domain.enuns.TypeClient;
 import com.luanoliveira.cursomc.dto.ClientNewDTO;
+import com.luanoliveira.cursomc.repositories.ClientRepository;
 import com.luanoliveira.cursomc.resources.exceptions.FieldMessage;
 import com.luanoliveira.cursomc.services.validation.utils.BR;
 
 public class ClientInsertValidator implements ConstraintValidator<ClientInsert, ClientNewDTO> {
+	
+	@Autowired
+	private ClientRepository repo;
 	
 	@Override
 	public void initialize(ClientInsert ann) {
@@ -30,11 +37,19 @@ public class ClientInsertValidator implements ConstraintValidator<ClientInsert, 
 			list.add(new FieldMessage("cpfOrCnpj", "CNPJ Inválido"));
 		}
 		
+		Client aux = repo.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
+		}
+		
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
 			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
 					.addConstraintViolation();
 		}
+		
+		
+		
 		return list.isEmpty();
 	}
 }
