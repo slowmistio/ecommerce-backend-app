f47931730e4ae4647cd9535627bc59303ed89537
Luan Oliveira <luannn@gmail.com>
2018-06-02 11:18:00 -0300

    Implementando autorizacao

--- src/main/java/com/luanoliveira/cursomc/security/JWTAuthenticationFilter.java
+++ src/main/java/com/luanoliveira/cursomc/security/JWTAuthenticationFilter.java
@@ -31,7 +31,7 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 	
 	@Override
 	public Authentication attemptAuthentication(HttpServletRequest req,
-											   HttpServletResponse res) throws AuthenticationException {
+											    HttpServletResponse res) throws AuthenticationException {
 		
 		try {
 			CredentialsDTO creds = new ObjectMapper()
