ba5ed2876ecfa1d89ce1654df2887003cac5ebad
Luan Oliveira <luannn@gmail.com>
2018-06-02 14:54:12 -0300

    Salvando o primeiro arquivo no S3

--- src/main/java/com/luanoliveira/cursomc/InitialApp.java
+++ src/main/java/com/luanoliveira/cursomc/InitialApp.java
@@ -1,13 +1,26 @@
 package com.luanoliveira.cursomc;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class InitialApp {
+import com.luanoliveira.cursomc.services.S3Service;
 
+@SpringBootApplication
+public class InitialApp implements CommandLineRunner {
+
+	@Autowired
+	private S3Service s3service;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(InitialApp.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		s3service.uploadFile("/home/hunter/Imagens/teste.jpeg");
+		
 	}
 	
 	
