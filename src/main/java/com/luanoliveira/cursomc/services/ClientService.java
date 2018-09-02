ded2bfcfbe88982d73715c466a69654a22e93100
Luan Eli Oliveira <luannn@gmail.com>
2018-09-02 12:39:38 -0300

    Ajustando tamanho da imagem do profile

--- src/main/java/com/luanoliveira/cursomc/services/ClientService.java
+++ src/main/java/com/luanoliveira/cursomc/services/ClientService.java
@@ -52,6 +52,9 @@ public class ClientService {
 	@Value("${img.prefix.client.profile}")
 	private String prefix;
 	
+	@Value("${img.profile.size}")
+	private Integer	 size;
+	
 	public Client findById(Integer clientId) {
 		
 		UserSS user = UserService.authenticated();
@@ -128,8 +131,9 @@ public class ClientService {
 		}
 		
 		BufferedImage jpgImage = imageService.getJPGImagemFromFile(multipartFile);
+		jpgImage = imageService.cropSquare(jpgImage);
+		jpgImage = imageService.resize(jpgImage, size);
 		
-		URI uri = s3Service.uploadFile(multipartFile);
 		String fileName = prefix + user.getId() + ".jpg";
 		
 		return s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
